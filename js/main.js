f95e62875fad23f5215648da2db81aa8bff5093c
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-14 15:00:14 +0100

    add const for img create cicle for (0-4) img

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-alert("funziona");
+/* MILESTONE 1
+
+Per prima cosa, creiamo il markup statico:
+costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+MILESTONE 2
+
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
+Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+MILESTONE 3
+
+Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. */
+
+/* strumenti usati 
+const
+let
+cicle for
+.length
+console.log
+ */
+
+/* creiamo una costante con all'interno le img da inserire nella Dom */
+const images = [
+  "../assets/img/01.webp",
+  "../assets/img/02.webp",
+  "../assets/img/03.webp",
+  "../assets/img/04.webp",
+  "../assets/img/05.webp",
+];
+
+// selezione immagine attiva
+let activeImage = 0;
+
+//seleziono immagine dom
+const imagesElement = document.querySelector(".slider > .images");
+
+// loop for ciclare immagini ( 0 - 4 )
+for (let i = 0; i < images.length; i++) {
+  const imgSrc = images[i];
+  const imgElement = `<img class="img-fluid ${
+    i === activeImage ? "active" : ""
+  }" src="${imgSrc}" alt="">`;
+  console.log(imgElement);
+  //insertAdjacentHTML
+  imagesElement.insertAdjacentHTML("beforeend", imgElement);
+}
