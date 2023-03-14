0bd7dc9c902dd4548fff562f4310665067b1da82
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-14 15:31:45 +0100

    add prev and next

--- js/main.js
+++ js/main.js
@@ -18,29 +18,82 @@ cicle for
 .length
 console.log
  */
-
-/* creiamo una costante con all'interno le img da inserire nella Dom */
+/********************************************************************************************************************/
+/********************************************************************************************************************/
+/* creo una costante per le img */
 const images = [
-  "../assets/img/01.webp",
-  "../assets/img/02.webp",
-  "../assets/img/03.webp",
-  "../assets/img/04.webp",
-  "../assets/img/05.webp",
+  "./assets/img/01.webp",
+  "./assets/img/02.webp",
+  "./assets/img/03.webp",
+  "./assets/img/04.webp",
+  "./assets/img/05.webp",
 ];
 
-// selezione immagine attiva
+// seleziono l'immagine attiva a schermo
 let activeImage = 0;
 
-//seleziono immagine dom
+// seleziono immagine dom
 const imagesElement = document.querySelector(".slider > .images");
 
-// loop for ciclare immagini ( 0 - 4 )
+// loop for usato per creare un ciclo di immagini
 for (let i = 0; i < images.length; i++) {
   const imgSrc = images[i];
   const imgElement = `<img class="img-fluid ${
     i === activeImage ? "active" : ""
   }" src="${imgSrc}" alt="">`;
   console.log(imgElement);
+
   //insertAdjacentHTML
   imagesElement.insertAdjacentHTML("beforeend", imgElement);
 }
+
+// seleziono tutte le slides
+const slideImagesElements = document.querySelectorAll(
+  ".slider > .images > img"
+);
+
+// aggiungo listen click next
+const nextEl = document.querySelector(".next");
+nextEl.addEventListener("click", function () {
+  console.log("cliccato next");
+
+  // seleziono la slide corrente
+  const currentSlide = slideImagesElements[activeImage];
+  console.log(currentSlide);
+
+  // rimuovo active class dreom the active image
+  currentSlide.classList.remove("active");
+
+  // creo un incremento del valore di "activeimage" variabile
+  activeImage++;
+
+  // seleziono la prossima slide
+  const nextImage = slideImagesElements[activeImage];
+
+  // attivo la classe " active" sulla prossima img
+  console.log(nextImage);
+  nextImage.classList.add("active");
+});
+
+// aggiungo listen click prev
+const prevEl = document.querySelector(".prev");
+prevEl.addEventListener("click", function () {
+  console.log("cliccato prev");
+
+  // select current slide
+  const currentSlide = slideImagesElements[activeImage];
+  console.log(currentSlide);
+
+  // remove active class dreom the active image
+  currentSlide.classList.remove("active");
+
+  //  creo un decremento del valore di "activeimage" variabile
+  activeImage--;
+
+  // seleziono la prossima slide
+  const nextImage = slideImagesElements[activeImage];
+
+  // attivo la classe " active" sulla prossima img
+  console.log(nextImage);
+  nextImage.classList.add("active");
+});
